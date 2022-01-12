3e5c56a4f415259241c6192ab9e2fb9a48b89858
Braden Dickerson <purpledragon6445@gmail.com>
2022-01-12 15:27:37 -0700

    Progress

--- arrayMethods.js
+++ arrayMethods.js
@@ -17,10 +17,10 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-// CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+ 
+const evenNumbers = mixedNumbers.filter(element => element % 2 === 0)                   // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
-
+console.log (filteredNums)
 
 ////////// PROBLEM 2 //////////
 
@@ -38,8 +38,10 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-// CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+
+const postTaxPrices = prices.map(function(num, ind, arr){
+  return postTaxPrices = prices * 0.07
+})// = prices.map(/* Provide Your Callback Here );
 
 
 
